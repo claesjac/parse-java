51122fca11091492e93ac4a83e25261ba3d0c896
Claes Jakobsson <claes@surfar.nu>
2012-02-10 21:22:55 +0100

    Parsing of modifiers

---
+++ lib/Parse/Java/Modifiers.pm
@@ -0,0 +1,15 @@
+package Parse::Java::Modifiers;
+
+use strict;
+use warnings;
+
+use base qw(Parse::Java::Node);
+
+1;
+__END__
+
+=head1 NAME
+
+Parse::Java::Modifiers - Node representing all modifiers for a entity
+
+=cut
--- t/java-src/TestInterface.java
+++ t/java-src/TestInterface.java
@@ -1,2 +1,2 @@
-public interface TestInterface {
+public interface TestInterface extends MyOtherInterface {
 }
